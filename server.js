f14ab654ea6e2aa8b967b3e019dee90d6ccaf13f
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-31 14:24:55 +0600

    Server connected with DB.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+const { connectDB } = require('./db/connect')
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json())
@@ -11,6 +12,15 @@ app.get('/',(req,res) => {
 
 app.use('/api/v1/tasks', tasks)
 
-app.listen(PORT,() => {
-    console.log(`Running at Port ${PORT}`);
-})
+const start = async () => {
+    try{
+        await connectDB()
+        app.listen(PORT,() => {
+            console.log(`Running at Port ${PORT}`);
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start()
