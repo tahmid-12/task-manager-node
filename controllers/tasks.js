e79b4e3666b9f7f99fe4dc7e993f6d04a6f56f81
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-31 23:00:42 +0600

    Delete Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -39,8 +39,19 @@ const updateTask = (req, res) => {
     res.send('Update task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('Delete task')
+const deleteTask = async (req, res) => {
+    try{
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID })
+
+        if(!task){
+            return res.status(404).json({ message: `Task Not Avaialble with ${taskID}`})
+        }
+
+        res.status(200).json({ task })
+    }catch(error){
+        res.status(500).json({ message: error })
+    }
 }
 
 module.exports = {
